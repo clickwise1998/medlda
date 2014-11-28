8617465a44969c7c8abc8735801d5cd7547d3b46
clickwise <strongmind666@126.com>
2014-11-28 16:53:43 +0800

    medlda src

--- src/cn/clickwise/medlda/MedLDA.java
+++ src/cn/clickwise/medlda/MedLDA.java
@@ -51,39 +51,36 @@ public class MedLDA {
 	public double doc_e_step(Document doc, double[] gamma, double[][] phi,
 			SuffStats ss, Params param) {
 
-		//posterior inference
-		double lhood=inference(doc,ss.getNum_docs(),gamma,phi,param);
-		
-		//update sufficient statistics
+		// posterior inference
+		double lhood = inference(doc, ss.getNum_docs(), gamma, phi, param);
+
+		// update sufficient statistics
 		double gamma_sum = 0;
-		for(int k=0;k<m_nK;k++)
-		{
-			gamma_sum+=gamma[k];
-			ss.alpha_suffstats[k]+=Utils.digamma(gamma[k]);
+		for (int k = 0; k < m_nK; k++) {
+			gamma_sum += gamma[k];
+			ss.alpha_suffstats[k] += Utils.digamma(gamma[k]);
 		}
-		
-		for(int k=0;k<m_nK;k++)
-		{
-			ss.alpha_suffstats[k]-=Utils.digamma(gamma_sum);
+
+		for (int k = 0; k < m_nK; k++) {
+			ss.alpha_suffstats[k] -= Utils.digamma(gamma_sum);
 		}
-		
-		for(int k=0;k<m_nK;k++)
-		{
-			double dVal=0;
-			for(int n=0;n<doc.getLength();n++)
-			{
-				ss.class_word[k][doc.words[n]]+=doc.counts[n]*phi[n][k];
-				ss.class_total[k]+=doc.counts[n]*phi[n][k];
-				dVal+=phi[n][k]*(double)doc.counts[n]/(double)doc.getTotal();
+
+		for (int k = 0; k < m_nK; k++) {
+			double dVal = 0;
+			for (int n = 0; n < doc.getLength(); n++) {
+				ss.class_word[k][doc.words[n]] += doc.counts[n] * phi[n][k];
+				ss.class_total[k] += doc.counts[n] * phi[n][k];
+				dVal += phi[n][k] * (double) doc.counts[n]
+						/ (double) doc.getTotal();
 			}
-			
-			//suff-stats for supervised LDA
-			ss.exp[ss.num_docs][k]=dVal;
-			
+
+			// suff-stats for supervised LDA
+			ss.exp[ss.num_docs][k] = dVal;
+
 		}
-		
-		ss.num_docs=ss.num_docs+1;
-			
+
+		ss.num_docs = ss.num_docs + 1;
+
 		return lhood;
 	}
 
@@ -94,11 +91,12 @@ public class MedLDA {
 
 	public int run_em(String start, String directory, Corpus corpus,
 			Params param) {
+		
 		m_dDeltaEll = param.getDELTA_ELL();
 
 		int d, n;
 
-		long runtime_start=TimeOpera.getCurrentTimeLong();
+		long runtime_start = TimeOpera.getCurrentTimeLong();
 		double[][] var_gamma = new double[corpus.num_docs][param.getNTOPICS()];
 
 		int max_length = corpus.max_corpus_length();
@@ -151,8 +149,7 @@ public class MedLDA {
 					filename));
 
 			int i = 0;
-			double lhood=0, lhood_old = 0, converged = 1;
-	
+			double lhood = 0, lhood_old = 0, converged = 1;
 
 			while (((converged < 0) || (converged > param.getEM_CONVERGED() || (i <= 2)))
 					&& (i <= param.getEM_MAX_ITER())) {
@@ -196,46 +193,45 @@ public class MedLDA {
 				i++;
 
 			}
-			
-			long runtime_end=TimeOpera.getCurrentTimeLong();
-			logger.info("Training time in (cpu-seconds):"+((double)runtime_end-(double)runtime_start)/1000.0);
-			
-			filename="MedLDA_("+m_nK+"topic)_train.txt";
-			
-			//output the final model
-			filename=directory+"/final";
+
+			long runtime_end = TimeOpera.getCurrentTimeLong();
+			logger.info("Training time in (cpu-seconds):"
+					+ ((double) runtime_end - (double) runtime_start) / 1000.0);
+
+			filename = "MedLDA_(" + m_nK + "topic)_train.txt";
+
+			// output the final model
+			filename = directory + "/final";
 			save_model(filename);
-			
-			filename=directory+"/final.gamma";
-            save_gamma(filename,var_gamma,corpus.num_docs,m_nK);		
-			
-			//output the word assignments(for visualization)
-	        int nNum=0,nAcc=0;
-	        filename=directory+"/word-assignments.dat";
-	        
-	        PrintWriter w_asgn_file=new PrintWriter(new FileWriter(filename));
-	        for(d=0;d<corpus.num_docs;d++)
-	        {
-	        	if((d%1000)==0)
-	        	{
-	        	   logger.info("final e step Document "+d);	
-	        	}
-	        	lhood+=inference(corpus.docs[d],d,var_gamma[d],phi,param);
-	        	write_word_assignment(w_asgn_file,corpus.docs[d],phi);
-	        	
-	        	nNum++;
-	        	predict(corpus.docs[d],phi);
-	        	
-	        	if(corpus.docs[d].getGndlabel()==corpus.docs[d].getPredlabel())
-	        	{
-	        		nAcc++;
-	        	}
-	        }
-            
-	        w_asgn_file.close();
-	        likelihood_file.close();
-	        logger.info("MedLDA: double count accuracy:"+(double)nAcc / nNum);
-            
+
+			filename = directory + "/final.gamma";
+			save_gamma(filename, var_gamma, corpus.num_docs, m_nK);
+
+			// output the word assignments(for visualization)
+			int nNum = 0, nAcc = 0;
+			filename = directory + "/word-assignments.dat";
+
+			PrintWriter w_asgn_file = new PrintWriter(new FileWriter(filename));
+			for (d = 0; d < corpus.num_docs; d++) {
+				if ((d % 1000) == 0) {
+					logger.info("final e step Document " + d);
+				}
+				lhood += inference(corpus.docs[d], d, var_gamma[d], phi, param);
+				write_word_assignment(w_asgn_file, corpus.docs[d], phi);
+
+				nNum++;
+				predict(corpus.docs[d], phi);
+
+				if (corpus.docs[d].getGndlabel() == corpus.docs[d]
+						.getPredlabel()) {
+					nAcc++;
+				}
+			}
+
+			w_asgn_file.close();
+			likelihood_file.close();
+			logger.info("MedLDA: double count accuracy:" + (double) nAcc / nNum);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -246,69 +242,159 @@ public class MedLDA {
 	public double infer(String model_dir, Corpus corpus, Params param,
 			String userId) {
 
-		return 0;
+		String filename;
+		int i, d, n;
+		double[][] var_gamma;
+		double lhood;
+		double[][] phi;
+		Document doc;
+
+		String model_root;
+		model_root = model_dir + "/final";
+		load_model(model_root);
+
+		// remove unseen words
+		if (corpus.num_terms > m_nNumTerms) {
+
+			for (i = 0; i < corpus.num_docs; i++) {
+				for (int k = 0; k < corpus.docs[i].length; k++) {
+					if (corpus.docs[i].words[k] >= m_nNumTerms) {
+						corpus.docs[i].words[k] = m_nNumTerms - 1;
+					}
+				}
+			}
+		}
+
+		var_gamma = new double[corpus.num_docs][m_nK];
+		double[][] exp = new double[corpus.num_docs][m_nK];
+
+		filename = model_dir + "/evl-lda-lhood.dat";
+		double dAcc=0;
+		
+		try {
+			PrintWriter fileptr = new PrintWriter(new FileWriter(filename));
+
+			for(d=0;d<corpus.num_docs;d++)
+			{
+				if(((d%1000)==0)&&(d>0))
+				{
+					logger.info("Document "+d);
+				}
+				
+				doc=corpus.docs[d];
+				phi=new double[doc.length][m_nK];
+				
+				for(n=0;n<doc.length;n++)
+				{
+					//initialize to uniform distribution
+					for(int k=0;k<m_nK;k++)
+					{
+						phi[n][k]=1.0;
+					}
+				}
+				
+				lhood=inference_pred(doc, var_gamma[d], phi, param);
+				
+				//do prediction
+				predict(doc,phi);
+				doc.lhood=lhood;
+				
+				fileptr.println(lhood);
+				
+				//update the exp
+				for(int k=0;k<m_nK;k++){
+					double dVal=0;
+					for(n=0;n<doc.length;n++){
+						dVal+=phi[n][k]*(double)doc.counts[n]/(double)doc.total;
+					}
+					
+					exp[d][k]=dVal;	
+				}					
+			}
+			
+			filename="MedLDA_("+m_nK+"topic)_test.txt";
+			outputData2(filename, corpus, exp, m_nK, m_nLabelNum);
+			
+			filename=model_dir+"/evl-gamma.dat";
+			save_gamma(filename,var_gamma,corpus.num_docs,m_nK);
+			
+			filename=model_dir+"/evl-performance.dat";
+		    dAcc=save_prediction(filename,corpus);
+			fileptr.close();
+			
+			fileptr=new PrintWriter(new FileWriter("overall-res.txt",true));
+			fileptr.printf("setup (K: %d; C: %.3f; fold: %d; ell: %.2f; dual-opt: %d; alpha: %d; svm_alg: %d; maxIt: %d): accuracy %.3f\n", m_nK, m_dC, param.NFOLDS, param.DELTA_ELL, param.PHI_DUALOPT, param.ESTIMATE_ALPHA, param.SVM_ALGTYPE, param.EM_MAX_ITER, dAcc);
+			
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return dAcc;
 	}
 
 	public double inference(Document doc, int docix, double[] var_gamma,
 			double[][] phi, Params param) {
-		
+
 		double converged = 1;
 		double phisum = 0, lhood = 0;
 		double lhood_old = 0;
-		double[] oldphi=new double[m_nK];
-		double[] digamma_gam=new double[m_nK];
-		
-		//compute poster for dirichlet
-		for(int k=0;k<m_nK;k++)
-		{
-			var_gamma[k]=m_alpha[k]+(double)doc.total/(double)m_nK;
-			digamma_gam[k]=Utils.digamma(var_gamma[k]);
-			for(int n=0;n<doc.length;n++)
-			{
-				phi[n][k]=1.0/(double)m_nK;
-			}	
+		double[] oldphi = new double[m_nK];
+		double[] digamma_gam = new double[m_nK];
+
+		// compute poster for dirichlet
+		for (int k = 0; k < m_nK; k++) {
+			var_gamma[k] = m_alpha[k] + (double) doc.total / (double) m_nK;
+			digamma_gam[k] = Utils.digamma(var_gamma[k]);
+			for (int n = 0; n < doc.length; n++) {
+				phi[n][k] = 1.0 / (double) m_nK;
+			}
 		}
-		
-		int var_iter=0;
-		while((converged > param.VAR_CONVERGED)&&((var_iter < param.VAR_MAX_ITER)
-                || (param.VAR_MAX_ITER == -1)))
-		{
-			 var_iter ++;
-			 for(int n=0;n<doc.length;n++)
-			 {
-				 phisum=0;
-				 
-				 for(int k=0;k<m_nK;k++)
-				 {
-					 oldphi[k] = phi[n][k];
-					 
-					 /* update the phi: add additional terms here for supervised LDA */
-					 double dVal = compute_mrgterm(doc, docix, n, k, param);
-					 phi[n][k]=digamma_gam[k] +m_dLogProbW[k][doc.words[n]]+dVal;
-					 
-					 if(k>0) phisum=Utils.log_sum(phisum,phi[n][k]);
-					 else   phisum=phi[n][k];
-				 }
-				 
-				 //update gamma and normalize phi
-				 for(int k=0;k<m_nK;k++)
-				 {
-					 phi[n][k]=Math.exp(phi[n][k]-phisum);
-					 var_gamma[k]=var_gamma[k]+doc.counts[n]*(phi[n][k]-oldphi[k]);
-					 
-					 digamma_gam[k]=Utils.digamma(var_gamma[k]);
-				 } 
-			 }
-			 lhood=compute_lhood(doc,phi,var_gamma);
-			 converged = (lhood_old - lhood) / lhood_old;
-			 lhood_old = lhood;	
+
+		int var_iter = 0;
+		while ((converged > param.VAR_CONVERGED)
+				&& ((var_iter < param.VAR_MAX_ITER) || (param.VAR_MAX_ITER == -1))) {
+			var_iter++;
+			for (int n = 0; n < doc.length; n++) {
+				phisum = 0;
+
+				for (int k = 0; k < m_nK; k++) {
+					oldphi[k] = phi[n][k];
+
+					/*
+					 * update the phi: add additional terms here for supervised
+					 * LDA
+					 */
+					double dVal = compute_mrgterm(doc, docix, n, k, param);
+					phi[n][k] = digamma_gam[k] + m_dLogProbW[k][doc.words[n]]
+							+ dVal;
+
+					if (k > 0)
+						phisum = Utils.log_sum(phisum, phi[n][k]);
+					else
+						phisum = phi[n][k];
+				}
+
+				// update gamma and normalize phi
+				for (int k = 0; k < m_nK; k++) {
+					phi[n][k] = Math.exp(phi[n][k] - phisum);
+					var_gamma[k] = var_gamma[k] + doc.counts[n]
+							* (phi[n][k] - oldphi[k]);
+
+					digamma_gam[k] = Utils.digamma(var_gamma[k]);
+				}
+			}
+			lhood = compute_lhood(doc, phi, var_gamma);
+			converged = (lhood_old - lhood) / lhood_old;
+			lhood_old = lhood;
 		}
-		
+
 		return 0;
 	}
 
 	/**
 	 * Given the model and w, compute the E[Z] for prediction
+	 * 
 	 * @param doc
 	 * @param var_gamma
 	 * @param phi
@@ -317,58 +403,57 @@ public class MedLDA {
 	 */
 	public double inference_pred(Document doc, double[] var_gamma,
 			double[][] phi, Params param) {
-		
-	    double converged = 1;
-	    double phisum = 0, lhood = 0;
-	    double lhood_old = 0;
-	    double[] oldphi=new double[m_nK];
-	    int k,n,var_iter;
-	    double[] digamma_gam=new double[m_nK];
-	    
-	    //compute posterior dirichlet
-	    for(k=0;k<m_nK;k++){
-	    	var_gamma[k]=m_alpha[k]+(double)doc.total/(double)m_nK;
-	    	digamma_gam[k]=Utils.digamma(var_gamma[k]);
-	    	for(n=0;n<doc.length;n++)
-	    	{
-	    		phi[n][k]=1.0/m_nK;
-	    	}
-	    	
-	    }
-	    
-	    var_iter=0;
-	    
-	    while((converged>param.VAR_CONVERGED)&&((var_iter<param.VAR_MAX_ITER)||(param.VAR_MAX_ITER==-1)))
-	    {
-	    	var_iter++;
-	    	for(n=0;n<doc.length;n++)
-	    	{
-	    		phisum=0;
-	    		for(k=0;k<m_nK;k++){
-	    			oldphi[k]=phi[n][k];
-	    			
-	    			phi[n][k]= digamma_gam[k] + m_dLogProbW[k][doc.words[n]];
 
-	    			if(k>0) phisum=Utils.log_sum(phisum,phi[n][k]);
-	    			else phisum=phi[n][k];
-	    		}
-	    		
-	    		//update gamma and normalize phi
-	    		for(k=0;k<m_nK;k++)
-	    		{
-	    			phi[n][k]=Math.exp(phi[n][k]-phisum);
-	    			var_gamma[k]=var_gamma[k]+doc.counts[n]*(phi[n][k]-oldphi[k]);
-	    			digamma_gam[k]=Utils.digamma(var_gamma[k]);
-	    		}
-	    		
-	    	}
-	    	
-            lhood = compute_lhood(doc, phi, var_gamma);
-            converged = (lhood_old - lhood) / lhood_old;
-            lhood_old = lhood;
-	    		
-	    }
-		
+		double converged = 1;
+		double phisum = 0, lhood = 0;
+		double lhood_old = 0;
+		double[] oldphi = new double[m_nK];
+		int k, n, var_iter;
+		double[] digamma_gam = new double[m_nK];
+
+		// compute posterior dirichlet
+		for (k = 0; k < m_nK; k++) {
+			var_gamma[k] = m_alpha[k] + (double) doc.total / (double) m_nK;
+			digamma_gam[k] = Utils.digamma(var_gamma[k]);
+			for (n = 0; n < doc.length; n++) {
+				phi[n][k] = 1.0 / m_nK;
+			}
+
+		}
+
+		var_iter = 0;
+
+		while ((converged > param.VAR_CONVERGED)
+				&& ((var_iter < param.VAR_MAX_ITER) || (param.VAR_MAX_ITER == -1))) {
+			var_iter++;
+			for (n = 0; n < doc.length; n++) {
+				phisum = 0;
+				for (k = 0; k < m_nK; k++) {
+					oldphi[k] = phi[n][k];
+
+					phi[n][k] = digamma_gam[k] + m_dLogProbW[k][doc.words[n]];
+
+					if (k > 0)
+						phisum = Utils.log_sum(phisum, phi[n][k]);
+					else
+						phisum = phi[n][k];
+				}
+
+				// update gamma and normalize phi
+				for (k = 0; k < m_nK; k++) {
+					phi[n][k] = Math.exp(phi[n][k] - phisum);
+					var_gamma[k] = var_gamma[k] + doc.counts[n]
+							* (phi[n][k] - oldphi[k]);
+					digamma_gam[k] = Utils.digamma(var_gamma[k]);
+				}
+
+			}
+
+			lhood = compute_lhood(doc, phi, var_gamma);
+			converged = (lhood_old - lhood) / lhood_old;
+			lhood_old = lhood;
+
+		}
 
 		return 0;
 	}
@@ -382,42 +467,73 @@ public class MedLDA {
 	public double compute_lhood(Document doc, double[][] phi, double[] var_gamma) {
 
 		double lhood = 0, digsum = 0, var_gamma_sum = 0, alpha_sum = 0;
-		double[] dig=new double[m_nK];
-		
-		for(int k=0;k<m_nK;k++){
-			dig[k]=Utils.digamma(var_gamma[k]);
-			var_gamma_sum+=var_gamma[k];
-			alpha_sum+=m_alpha[k];
+		double[] dig = new double[m_nK];
+
+		for (int k = 0; k < m_nK; k++) {
+			dig[k] = Utils.digamma(var_gamma[k]);
+			var_gamma_sum += var_gamma[k];
+			alpha_sum += m_alpha[k];
 		}
-		
-		digsum=Utils.digamma(var_gamma_sum);
-		
-		lhood = Utils.lgamma( alpha_sum ) - (Utils.lgamma(var_gamma_sum));
-		for(int k=0;k<m_nK;k++)
-		{
-			lhood-=Utils.lgamma(m_alpha[k]);
+
+		digsum = Utils.digamma(var_gamma_sum);
+
+		lhood = Utils.lgamma(alpha_sum) - (Utils.lgamma(var_gamma_sum));
+		for (int k = 0; k < m_nK; k++) {
+			lhood -= Utils.lgamma(m_alpha[k]);
 		}
-		
-		for(int k=0;k<m_nK;k++)
-		{
-			lhood+=(m_alpha[k]-1)*(dig[k]-digsum)+Utils.lgamma(var_gamma[k])-(var_gamma[k]-1)*(dig[k]-digsum);
-			
-			double dVal=0;
-			for(int n=0;n<doc.length;n++)
-			{
-				if(phi[n][k]>0){
-					lhood+=doc.counts[n]*(phi[n][k]*((dig[k]-digsum)-Math.log(phi[n][k])+m_dLogProbW[k][doc.words[n]]));
+
+		for (int k = 0; k < m_nK; k++) {
+			lhood += (m_alpha[k] - 1) * (dig[k] - digsum)
+					+ Utils.lgamma(var_gamma[k]) - (var_gamma[k] - 1)
+					* (dig[k] - digsum);
+
+			double dVal = 0;
+			for (int n = 0; n < doc.length; n++) {
+				if (phi[n][k] > 0) {
+					lhood += doc.counts[n]
+							* (phi[n][k] * ((dig[k] - digsum)
+									- Math.log(phi[n][k]) + m_dLogProbW[k][doc.words[n]]));
 				}
-				dVal+=phi[n][k]*doc.counts[n]/doc.total;
+				dVal += phi[n][k] * doc.counts[n] / doc.total;
 			}
 		}
-		
+
 		return 0;
 	}
 
 	public void predict(Document doc, double[][] phi) {
+		doc.predlabel = -1;
+		double dMaxScore = 0;
+		for (int y = 0; y < m_nLabelNum; y++) {
+			double dScore = 0;
+			for (int k = 0; k < m_nK; k++) {
+				int etaIx = y * m_nK + k;
+
+				double dVal = 0;
+				for (int n = 0; n < doc.length; n++) {
+					dVal += phi[n][k] * (double) doc.counts[n]
+							/ (double) doc.total;
+				}
+
+				dScore += dVal * m_dEta[etaIx];
+			}
+			dScore -= m_dB;
+			if (doc.predlabel == -1 || dScore > dMaxScore) {
+				doc.predlabel = y;
+				dMaxScore = dScore;
+			}
+
+		}
 
 	}
+	
+	public double save_prediction(String filename, Corpus corpus)
+	{
+		
+		return 0;
+	}
+	
+
 
 	public void loss_aug_predict(Document doc, double[] zbar_mean) {
 
@@ -465,17 +581,25 @@ public class MedLDA {
 	public void save_model(String model_root) {
 
 	}
-	
-	public void save_gamma(String filename,double[][] gamma,int num_docs,int num_topics)
-	{
-		
-	}
-	
-	public void write_word_assignment(PrintWriter f,Document doc,double[][] phi)
-	{
-		
+
+	public void save_gamma(String filename, double[][] gamma, int num_docs,
+			int num_topics) {
+
 	}
 
+	public void write_word_assignment(PrintWriter f, Document doc,
+			double[][] phi) {
+
+	}
+
+	public void outputData2(String filename,Corpus corpus,double[][] exp,int ntopic,int nLabels)
+	{
+		
+		
+		
+	}
+	
+	
 	public double[] getM_alpha() {
 		return m_alpha;
 	}
