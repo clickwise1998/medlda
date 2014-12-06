98206dbdf0897f14952e768a10d152b0b78d4e15
bluesky1999 <strongmind666@126.com>
2014-12-06 15:48:33 +0800

    run test

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@
 #java -Xmx2000m -cp jmlt.jar org.jmlp.classify.svm_struct.source.svm_struct_classify example/test.txt example/model example/prediction
 
 
-java -Xmx100m -cp out/medlda.jar cn.clickwise.medlda.Main est 120 4 10 16 64 temp random
+#java -Xmx100m -cp out/medlda.jar cn.clickwise.medlda.Main est 100 4 10 16 64 temp random
 
 
 java -cp out/medlda.jar cn.clickwise.medlda.Main inf 4 temp100_c16_f10
