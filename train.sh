3850f66efed0b41d5725a5ff4486a2c695ccb794
lq <liqi_haiyang@126.com>
2014-12-26 18:02:37 +0800

    test

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#java -Xmx15000m -cp out/medlda.jar cn.clickwise.classify.svm_struct_pattern.svm_struct_main -c 5000 tbmul_1224/nltrain_tb_1224.txt tbmul_1224/model_1224
+java -Xmx15000m -cp out/medlda.jar cn.clickwise.classify.svm_struct_pattern.svm_struct_main -c 5000 tbng/ntrain_ng.txt tbng/model_ng
 
-java -Xmx10000m -cp out/medlda.jar cn.clickwise.classify.svm_struct_pattern.svm_struct_classify tbmul_1224/nltest_tb_1224.txt tbmul_1224/model_1224 tbmul_1224/prediction
+#java -Xmx10000m -cp out/medlda.jar cn.clickwise.classify.svm_struct_pattern.svm_struct_classify tbng/ntest_ng.txt tbng/model_ng tbng/prediction
 
 
-#java -Xmx100m -cp out/medlda.jar cn.clickwise.medlda.Main est 110 4 10 16 64 temp random
+#java -Xmx10000m -cp out/medlda.jar cn.clickwise.medlda.Main est 120 566 10 16 64 temp random
 
 
 #java -cp out/medlda.jar cn.clickwise.medlda.Main inf 4 temp110_c16_f10
