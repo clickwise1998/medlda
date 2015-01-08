54d68b1fef7fb8d93776a9b749e5e1dc4b21ffa2
lq <liqi_haiyang@126.com>
2015-01-08 14:13:24 +0800

    test

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#java -Xmx15000m -cp out/medlda.jar cn.clickwise.classify.sspm.svm_struct_main -c 5000 tb_1224_ngram/nltrain_1224_ngram.txt tb_1224_ngram/model_1224_lngram
+#java -Xmx15000m -cp out/medlda.jar cn.clickwise.classify.sspm.svm_struct_main -c 5000 metrics_NN_NR_L5/ntrainSample_NN_NR_L5.txt metrics_NN_NR_L5/model_NN_NR_L5
 
-#java -Xmx10000m -cp out/medlda.jar cn.clickwise.classify.sspm.svm_struct_classify tb_1224_ngram/nltest_1224_ngram.txt tb_1224_ngram/model_1224_lngram tb_1224_ngram/prediction
+#java -Xmx10000m -cp out/medlda.jar cn.clickwise.classify.sspm.svm_struct_classify metrics_NN_NR_L5/ntestSample_NN_NR_L5.txt metrics_NN_NR_L5/model_NN_NR_L5 metrics_NN_NR_L5/prediction
 
 #java -Xmx15000m -cp out/medlda.jar cn.clickwise.classify.sspm.svm_struct_main -c 5000 example/train.txt example/model
 
-java -Xmx10000m -cp out/medlda.jar cn.clickwise.classify.sspm.svm_struct_classify example/test.txt example/model example/prediction
+#java -Xmx10000m -cp out/medlda.jar cn.clickwise.classify.sspm.svm_struct_classify example/test.txt example/model example/prediction
 
 
-#java -Xmx100m -cp out/medlda.jar cn.clickwise.medlda.Main est 110 4 10 16 64 temp random
+java -Xmx10000m -cp out/medlda.jar cn.clickwise.medlda.Main est 120 29 10 16 64 temp random
 
 
-#java -cp out/medlda.jar cn.clickwise.medlda.Main inf 4 temp110_c16_f10
+#java -cp out/medlda.jar cn.clickwise.medlda.Main inf 29 temp120_c16_f10
