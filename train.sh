b8adc7e121dde3ef62a2b7d6f52b3c1a71fcf4de
lq <liqi_haiyang@126.com>
2015-01-11 20:54:50 +0800

    run test

--- train.sh
+++ train.sh
@@ -8,8 +8,8 @@
 #java -Xmx10000m -cp out/medlda.jar cn.clickwise.classify.sspm.svm_struct_classify example/test.txt example/model example/prediction
 
 
-#java -Xmx1500m -cp out/medlda.jar cn.clickwise.medlda.Main est 120 29 10 16 64 temp random
+java -Xmx1500m -cp out/medlda.jar cn.clickwise.medlda.Main est 120 29 10 16 64 temp random
 
 
 #java -Xmx5000m -cp out/medlda.jar cn.clickwise.medlda.Main inf 29 temp120_c16_f10 0.02 0.03 0.08 0.1 0.2 0.5 0.8 0.9 0.95 0.98
-java -Xmx5000m -cp out/medlda.jar cn.clickwise.medlda.Main inf 29 temp120_c16_f10
+#java -Xmx5000m -cp out/medlda.jar cn.clickwise.medlda.Main inf 29 temp120_c16_f10
