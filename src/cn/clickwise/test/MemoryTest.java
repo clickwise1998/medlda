936bdd4504285b3b1463a9d63d6c9ee7ca93dba5
clickwise <strongmind666@126.com>
2015-01-21 16:18:23 +0800

    run test

--- src/cn/clickwise/test/MemoryTest.java
+++ src/cn/clickwise/test/MemoryTest.java
@@ -5,17 +5,18 @@ public class MemoryTest {
 	public static void main(String[] args)
 	{
 		
-		if(args.length!=2)
+		if(args.length!=3)
 		{
-			System.err.println("Usage:<word num> <test type>" +
+			System.err.println("Usage:<word num> <test type> <word length>" +
 					"word num:声明的单词个数" +
-					"test type: 0不new 1 new");
-			
+					"test type: 0不new 1 new");		
 			System.exit(1);
 		}
 		
 		long wordNum=Long.parseLong(args[0]);
 		int type=Integer.parseInt(args[1]);
+		int len=Integer.parseInt(args[2]);
+		
 		int i=0;
 		while(true)
 		{
@@ -25,7 +26,7 @@ public class MemoryTest {
 				WORD[] test;
 				if(type==1)
 				{
-					test=new WORD[1];
+					test=new WORD[len];
 				}
 			}
 		}
