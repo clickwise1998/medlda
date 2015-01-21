b2f3be29016f8739a50ff148f0bab39f04db321c
clickwise <strongmind666@126.com>
2015-01-21 16:45:09 +0800

    run test

---
+++ src/cn/clickwise/test/MemoryTestW2.java
@@ -0,0 +1,32 @@
+package cn.clickwise.test;
+
+public class MemoryTestW2 {
+	public static void main(String[] args)
+	{
+		
+		if(args.length!=3)
+		{
+			System.err.println("Usage:<word num> <test type> <word length>" +
+					"word num:声明的单词个数" +
+					"test type: 0不new 1 new");		
+			System.exit(1);
+		}
+		
+		long wordNum=Long.parseLong(args[0]);
+		int type=Integer.parseInt(args[1]);
+		int len=Integer.parseInt(args[2]);
+		WORD2[] test;
+		int i=0;
+		while(true)
+		{
+			if(i<wordNum)
+			{
+				i++;
+				if(type==1)
+				{
+					test=new WORD2[len];
+				}
+			}
+		}
+	}
+}
---
+++ src/cn/clickwise/test/WORD2.java
@@ -0,0 +1,6 @@
+package cn.clickwise.test;
+
+public class WORD2 {
+	  public int wnum=0;
+	  public double weight=0;
+}
